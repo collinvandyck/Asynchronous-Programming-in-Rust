38388346cca9bbf102a94c303f4cdeb75b257a11
Collin Van Dyck <collinvandyck@gmail.com>
2024-02-14 22:45:29 -0500

    Use arm dereference.

--- ch01/a-assembly-dereference/src/main.rs
+++ ch01/a-assembly-dereference/src/main.rs
@@ -11,10 +11,23 @@ fn main() {
     println!("{}", x);
 }
 
-fn dereference(ptr: *const usize) -> usize {
+#[allow(unused)]
+fn dereference_x86_64(ptr: *const usize) -> usize {
     let mut res: usize;
     unsafe { 
         asm!("mov {0}, [{1}]", out(reg) res, in(reg) ptr)
     };
     res
 }
+
+fn dereference(ptr: *const usize) -> usize {
+    let mut res: usize;
+    unsafe {
+        // Using `ldr` instruction for ARM64 to load the value from a pointer into a register.
+        // Note: Inline assembly syntax and registers might need adjustments based on
+        // specific use cases and compiler versions.
+        asm!("ldr {res}, [{ptr}]", res = out(reg) res, ptr = in(reg) ptr)
+    };
+    res
+}
+
